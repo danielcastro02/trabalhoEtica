f3967b1069fa8e9cab142ff55ae5e2f0d490bc11
Daniel <danielcastro02>
2019-10-24 15:03:17 -0300

    asdfasfd

--- index.php
+++ index.php
@@ -14,7 +14,12 @@ if (!isset($_SESSION)) {
     <title>Etica</title>
 <body class="homeimg">
 <main>
-
+<div class="row">
+    <div class="center card col s10 offset-s1">
+        <h5>Bem vindo a nossa histórinha</h5>
+        <p>Olá, </p>
+    </div>
+</div>
 </main>
 <?php
 include_once './Base/footer.php';
